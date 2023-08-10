93114071a4c48d4c1e762f2377a1f51db9f303e7
Eric Aguirrez Andraca <ericaguirrez@hotmail.com>
2023-08-10 16:38:50 -0600

    Update script.js
    Updated version of the portfolio

--- script.js
+++ script.js
@@ -1,13 +1,60 @@
+const downloadButton = document.querySelectorAll('.download-cv');
+const socialsButton = document.querySelector('.filled');
+const touchButton = document.getElementById('touch');
+const inTouch = document.getElementById('touch').innerText;
+const inTouchWeight = touchButton.style.fontWeight;
 
-window.addEventListener('scroll', function(){
-    let arrowUp = document.querySelector('.arrow-up');
-    let inicio = document.querySelector('.header');
-    let position = inicio.getBoundingClientRect().top;
-    let windowHeight = window.innerHeight;
+downloadButton.forEach(download => {
+    download.addEventListener('click', () => {
+        const fileUrl = './cv.pdf';
+        const fileName = 'cv.pdf';
+      
+        fetch(fileUrl)
+          .then(response => response.blob())
+          .then(blob => {
+            // Create a temporary anchor element
+            const a = document.createElement('a');
+            a.href = URL.createObjectURL(blob);
+            a.download = fileName;
+      
+            // Programmatically trigger the download
+            a.click();
+      
+            // Clean up
+            URL.revokeObjectURL(a.href);
+            a.remove();
+          });
+      });
+});
 
-    if (position != 0) {
-        arrowUp.classList.remove('hide');
-    } else {
-        arrowUp.classList.add('hide');
-    }
+
+
+socialsButton.addEventListener('click', () => {
+  let socialMedia = document.querySelector('.social__media');
+  if (socialMedia.classList.contains('hide')) {
+    socialMedia.style.zIndex = '-1';
+    socialMedia.classList.remove ('hide');
+    socialMedia.style.animation = 'appear 1s linear';
+    setTimeout(() => {
+      socialMedia.style.zIndex = '1';
+    }, 990);
+  } else {
+    socialMedia.style.zIndex = '-1';
+    socialMedia.style.animation = 'dissappear 1s linear';
+    setTimeout(() => {
+      socialMedia.classList.add ('hide');
+    }, 990);
+  }
+});
+
+
+
+touchButton.addEventListener('mouseover', () => {
+  touchButton.innerText = 'Whatsapp me';
+  touchButton.style.fontWeight = '800';
+});
+
+touchButton.addEventListener('mouseout', () => {
+  touchButton.innerText = inTouch;
+  touchButton.style.fontWeight = inTouchWeight;
 });
